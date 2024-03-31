6f89d72a4771970ac76384f27dc7005003713d8f
jaakop <jaakko@kronogra.fi>
2024-03-31 19:50:41 +0300

    Fishy canvas can now be created from the create menu

--- Runtime/FishyCanvas.cs
+++ Runtime/FishyCanvas.cs
@@ -50,35 +50,17 @@ namespace FishyUI
 
 #if UNITY_EDITOR
         [UnityEditor.MenuItem("GameObject/UI/Fishy UI/Canvas")]
-        private static void CreateNewButtonGameObject(UnityEditor.MenuCommand menuCommand)
+        private static void CreateNewCanvasGameObject(UnityEditor.MenuCommand menuCommand)
         {
             var go = new GameObject();
             go.AddComponent<RectTransform>();
-            go.AddComponent<CanvasRenderer>();
-            go.AddComponent<Image>();
-            go.transform.SetParent((menuCommand.context as GameObject).transform);
-            go.name = "Button";
-            (go.transform as RectTransform).anchoredPosition = Vector3.zero;
+            go.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            go.AddComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            go.AddComponent<GraphicRaycaster>();
 
-            var txtGo = new GameObject();
-            var txtTransform = txtGo.AddComponent<RectTransform>();
+            go.name = "Canvas";
 
-            txtGo.AddComponent<CanvasRenderer>();
-            txtGo.transform.SetParent(go.transform);
-            txtGo.name = "Text";
-
-            var txtC = txtGo.AddComponent<TextMeshProUGUI>();
-            txtC.text = "Button";
-            txtC.fontSize = 24;
-            txtC.alignment = TextAlignmentOptions.Center;
-            txtC.color = new Color(50 / 255f, 50 / 255f, 50 / 255f);
-
-            txtTransform.offsetMax = new Vector2(0, 0);
-            txtTransform.offsetMin = new Vector2(0, 0);
-            txtTransform.anchorMax = new Vector2(1, 1);
-            txtTransform.anchorMin = new Vector2(0, 0);
-
-            go.AddComponent<FishyButton>();
+            go.AddComponent<FishyCanvas>();
 
         }
 #endif
